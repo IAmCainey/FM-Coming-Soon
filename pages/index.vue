d67d362de61ed191f5a4689546f27d9ac430fee4
Paul Caine <paulcaine@tutanota.com>
2021-03-22 00:23:41 +0000

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="wrapper">
+    <div class="container">
+      <div class="content">
+        <img src="../assets/images/logo.svg" alt="Base Apparel">
+
+        <h1>
+          We're <br>
+          <span class="second__h1">coming
+            <br>soon</span>
+        </h1>
+
+        <p>
+          Hello fellow shoppers! We're currently building our new <br>
+          fashion store. Add your email below to stay up-to-date with <br>
+          announcements and our launch deals.
+        </p>
+
+        <form action="">
+          <input type="email" placeholder="Email Adress" name="email" required>
+          <button class="btn">
+            <img src="../assets/images/icon-arrow.svg" alt="">
+          </button>
+        </form>
+
+          <p class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            Coded by <a href="https://paulcaine.co.uk">Paul Caine</a>.
+          </p>
+
+      </div>
+
+      <div class="hero"></div>
+
+    </div>
+  </section>
+</template>
